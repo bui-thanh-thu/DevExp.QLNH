d075edea96b1e9895a75e67d69e0b7c72ea3bc50
thubt <buithanhthu127@gmail.com>
2020-08-28 14:42:47 +0700

    Add project files.

---
+++ DevExp.QLNH/Properties/ucOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace DevExp.QLNH.Properties
+{
+    public partial class ucOrder : DevExpress.XtraEditors.XtraUserControl
+    {
+        private static ucOrder _instance;
+        public static ucOrder Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new ucOrder();
+                return _instance;
+            }
+        }
+        public ucOrder()
+        {
+            InitializeComponent();
+        }
+    }
+}
